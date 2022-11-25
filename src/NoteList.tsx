236c106bcd2f43192547fa975a72c458907cdf79
chaosmos <vitovich1987@gmail.com>
2022-11-26 00:29:10 +0300

    finished project 🚀

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -55,7 +55,7 @@ function NoteList({
     <>
       <Row className="align-items-center mb-4">
         <Col>
-          <h1>Notes</h1>
+          <h1>Your Notes</h1>
         </Col>
         <Col xs="auto">
           <Stack gap={2} direction="horizontal">
